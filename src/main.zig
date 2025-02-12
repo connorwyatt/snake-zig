c5413180709fc19d3eeab3904d15be0c152f6fb1
Connor Wyatt <connorwyatt1@gmail.com>
2025-02-12 18:59:05 +0000

    feat: allow direction change and add fruit

--- src/main.zig
+++ src/main.zig
@@ -25,6 +25,7 @@ pub fn main() !void {
 
     rl.setTargetFPS(FRAME_RATE);
 
+    var score: u16 = 0;
     var game_over = false;
 
     var frame_modulus: u8 = 1;
@@ -49,7 +50,7 @@ pub fn main() !void {
     var snake_position = std.ArrayList(Vec2(i8)).init(allocator);
     defer snake_position.deinit();
 
-    const snake_direction = Vec2(i8).POS_X;
+    var snake_direction = Vec2(i8).POS_X;
 
     try snake_position.append(initial_snake_position);
     for (1..INITIAL_SNAKE_LENGTH) |i| {
@@ -61,39 +62,110 @@ pub fn main() !void {
 
     std.log.debug("Initial snake position: {any}", .{snake_position.items});
 
+    var fruit_positions = std.ArrayList(Vec2(i8)).init(allocator);
+    defer fruit_positions.deinit();
+
+    try addFruit(&random, &fruit_positions, snake_position.items);
+
     const font = try rl.getFontDefault();
+
+    var queued_direction: ?Vec2(i8) = null;
 
     while (!rl.windowShouldClose()) {
         if (!game_over) {
             frame_modulus = (frame_modulus + 1) % (FRAME_RATE / SNAKE_SPEED);
 
+            if (rl.isKeyPressed(rl.KeyboardKey.w)) {
+                queued_direction = Vec2(i8).NEG_Y;
+            }
+            if (rl.isKeyPressed(rl.KeyboardKey.s)) {
+                queued_direction = Vec2(i8).POS_Y;
+            }
+            if (rl.isKeyPressed(rl.KeyboardKey.a)) {
+                queued_direction = Vec2(i8).NEG_X;
+            }
+            if (rl.isKeyPressed(rl.KeyboardKey.d)) {
+                queued_direction = Vec2(i8).POS_X;
+            }
+
             if (frame_modulus == 0) {
-                _ = snake_position.pop();
+                if (queued_direction) |qd| {
+                    const is_axis_change = (snake_direction.x == 0 and qd.x != 0) or (snake_direction.y == 0 and qd.y != 0);
+
+                    if (is_axis_change) {
+                        snake_direction = qd;
+                    }
+                }
+                queued_direction = null;
+
+                const popped_position = snake_position.pop();
                 const head_position = snake_position.items[0];
                 try snake_position.insert(
                     0,
                     head_position.plus(&snake_direction),
                 );
-            }
 
-            const head_position = snake_position.items[0];
+                const new_head_position = snake_position.items[0];
 
-            if (head_position.x < 0 or
-                head_position.x > SNAKE_GRID_SIZE.x or
-                head_position.y < 0 or
-                head_position.y > SNAKE_GRID_SIZE.y)
-            {
-                game_over = true;
+                if (indexOfPosition(fruit_positions.items, &new_head_position)) |i| {
+                    score += 1;
+                    _ = fruit_positions.orderedRemove(i);
+
+                    try snake_position.append(popped_position);
+
+                    try addFruit(&random, &fruit_positions, snake_position.items);
+                }
+
+                if (new_head_position.x < 0 or
+                    new_head_position.x > SNAKE_GRID_SIZE.x or
+                    new_head_position.y < 0 or
+                    new_head_position.y > SNAKE_GRID_SIZE.y or
+                    indexOfPosition(snake_position.items[1..], &new_head_position) != null)
+                {
+                    game_over = true;
+                }
             }
         }
 
-        try draw(&font, snake_position.items, &game_over);
+        try draw(&font, snake_position.items, fruit_positions.items, &score, &game_over);
+    }
+}
+
+fn indexOfPosition(slice: []const Vec2(i8), position: *const Vec2(i8)) ?usize {
+    return for (slice, 0..) |item, i| {
+        if (std.meta.eql(item, position.*)) {
+            break i;
+        }
+    } else null;
+}
+
+fn addFruit(
+    random: *const std.Random,
+    fruit_positions: *std.ArrayList(Vec2(i8)),
+    snake_position: []const Vec2(i8),
+) !void {
+    outer_while: while (true) {
+        const random_x = random.intRangeLessThan(i8, 0, SNAKE_GRID_SIZE.x) + 1;
+        const random_y = random.intRangeLessThan(i8, 0, SNAKE_GRID_SIZE.y) + 1;
+        const random_position = Vec2(i8){ .x = random_x, .y = random_y };
+
+        if (indexOfPosition(snake_position, &random_position)) |_| {
+            continue :outer_while;
+        }
+        if (indexOfPosition(fruit_positions.items, &random_position)) |_| {
+            continue :outer_while;
+        }
+
+        try fruit_positions.append(random_position);
+        break;
     }
 }
 
 fn draw(
     font: *const rl.Font,
     snake_position: []const Vec2(i8),
+    fruit_positions: []const Vec2(i8),
+    score: *const u16,
     game_over: *const bool,
 ) !void {
     rl.beginDrawing();
@@ -114,14 +186,27 @@ fn draw(
         rl.Color.ray_white,
     );
 
+    var title_buffer: [20]u8 = undefined;
+    const title_slice = try std.fmt.bufPrintZ(&title_buffer, "Snake - Score: {}", .{score.*});
+
     rl.drawTextEx(
         font.*,
-        "Snake",
+        title_slice,
         rl.Vector2.init(CELL_SIZE.x + 8, 8),
         16,
         1,
         rl.Color.white,
     );
+
+    for (fruit_positions) |fp| {
+        rl.drawRectangle(
+            @as(i32, @intCast(fp.x)) * CELL_SIZE.x,
+            @as(i32, @intCast(fp.y)) * CELL_SIZE.y,
+            CELL_SIZE.x,
+            CELL_SIZE.y,
+            rl.Color.red,
+        );
+    }
 
     for (snake_position) |sp| {
         rl.drawRectangle(
